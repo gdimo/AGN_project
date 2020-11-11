078dae37aada3ef09e1b882e57fbc6d78d883281
G Dimopoulos <g.dimopoulos@hotmail.com>
2020-11-11 16:09:01 +0200

    NOT normalized distriburtions

--- code/distributions.py
+++ code/distributions.py
@@ -88,7 +88,7 @@ plt.xticks(fontsize=10, rotation=0)
 plt.tight_layout()
 
 
-plt.savefig('./figures/kt_both_dist.pdf',dpi=400)
+plt.savefig('./figures/kt_dist.pdf',dpi=400)
 
 #----------------OPTICAL DEPTH TWO FIGS-----------------------------------------------
 
@@ -99,32 +99,32 @@ fig, [ax1, ax2] = plt.subplots(2,1,figsize=(6,10))
 #plt.rcParams['xtick.top'] = True
 #plt.rcParams['ytick.right'] = True
 
-ax1.hist(go_tbl['Tau'],bins=7,histtype='step',color='b',density=1,
+ax1.hist(go_tbl['Tau'],bins=7,histtype='step',color='b',#density=1,
         alpha=1,linewidth=2, edgecolor='b', label=r'Constrained $kT_e$')
 
-ax1.hist(lo_tbl['Tau'],bins=7,histtype='step',color='k',density=1,
+ax1.hist(lo_tbl['Tau'],bins=7,histtype='step',color='k',#density=1,
         alpha=1,linewidth=2, linestyle='dotted' ,edgecolor='black', label='Low limit fixed values')
 
 ax1.tick_params(axis='both', which='major', labelsize=10, direction='in')
 #ax1.set_xticks(fontsize=15)
 #ax1.set_yticks([0.0,0.05,0.10,0.15,0.20,0.25,0.30,0.35],[0,5,10,15,20,25,30,35])
-ax1.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1,decimals=0))
+#ax1.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1,decimals=0))
 
 
 ax1.legend()
 
-ax2.bar(tcenter,tval_n,width=1.12,
+ax2.bar(tcenter,tval,width=1.12,
             color='none',alpha=1,edgecolor='black',linewidth=2,linestyle='dashed',
             label='KM estimator')
 
-ax2.hist(go_tbl['Tau'],bins=7,histtype='step',color='b',density=1,
+ax2.hist(go_tbl['Tau'],bins=7,histtype='step',color='b',#density=1,
         alpha=1,linewidth=2, edgecolor='b', label=r'Constrained $kT_e$')
 
 ax2.legend()
 #plt.grid(b=True, which='major', ls=':')
 plt.xlabel(r'Optical depth $\tau$',fontsize=15)
 ax2.tick_params(axis='both', which='major', labelsize=10, direction='in')
-ax2.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1,decimals=0))
+#ax2.yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1,decimals=0))
 
 
 plt.xticks(fontsize=10, rotation=0)
@@ -132,4 +132,4 @@ plt.xticks(fontsize=10, rotation=0)
 plt.tight_layout()
 
 
-plt.savefig('./figures/Tau_both_dist.pdf',dpi=400)
+plt.savefig('./figures/tau_dist.pdf',dpi=400)
