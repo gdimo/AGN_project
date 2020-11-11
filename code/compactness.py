934311a0b56f89112513ca83c87e68008b875575
G Dimopoulos <g.dimopoulos@hotmail.com>
2020-11-11 12:15:00 +0200

    create different lists for upper and lower limits

--- code/compactness.py
+++ code/compactness.py
@@ -670,12 +670,14 @@ plt.rcParams['ytick.right'] = True
 # Build the tables
 bin_meanu1 = np.array([-1.032, -0.963, -0.740, -1.014, -1.037])
 bin_centersu1 = np.array([0.83, 1.39, 1.66, 1.93, 2.42])
-bin_widthu1 = np.array([0.24, 0.3, 0.3, 0.3, 0.3])
+#bin_widthu1 = np.array([0.24, 0.3, 0.3, 0.3, (0.3,0.5)])
+ss_low = [0.12, 0.15, 0.15, 0.15, 0.3]
+ss_up = [0.12, 0.15, 0.15, 0.15, 1.0]
 bin_stdu1 = np.array([0.116, 0.08, 0.061, 0.067, 0.052])   
 ax2.plot(bin_meanu1,bin_centersu1,'ks',ms=10,label='KM sources')
 ax2.errorbar(bin_meanu1,bin_centersu1,
          xerr=bin_stdu1,
-         yerr=bin_widthu1,
+         yerr=[ss_low, ss_up],
          fmt='k,',
          capsize=3,
          ecolor='k',
